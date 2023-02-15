7bc9957f9bc8983a73fc46403103dafb4c8cd488
jbkoshy <koshyjeremy@gmail.com>
2023-02-14 22:42:50 -0600

    Updates

---
+++ precache-manifest.3aaf208c3ea4b841150bec6d87183d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da5df28e8bccf8002cccbf0abe33ec4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "151ce5d6c4b312409ecf",
+    "url": "/react-portfolio/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "075d6957fb6181070040",
+    "url": "/react-portfolio/static/css/main.140af143.chunk.css"
+  },
+  {
+    "revision": "151ce5d6c4b312409ecf",
+    "url": "/react-portfolio/static/js/2.be14716a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.be14716a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075d6957fb6181070040",
+    "url": "/react-portfolio/static/js/main.5931944c.chunk.js"
+  },
+  {
+    "revision": "ef573dc301c66cc2bd4c",
+    "url": "/react-portfolio/static/js/runtime-main.476b97d1.js"
+  }
+]);
